5575530594467707c771213831a3bc12b420facd
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-06 22:00:34 +0300

    Refactor versioniereDaten function to accept an optional callback for post-versioning actions.

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const fs = require('fs');
 const path = require('path');
 
 // Funktion zur Erfassung und Versionierung der Daten
-function versioniereDaten(daten) {
+function versioniereDaten(daten, callback) {
     // Logik zur Speicherung und Versionierung der Daten
     console.log('Daten wurden versioniert:', daten);
+    if (callback && typeof callback === 'function') {
+        callback();
+    }
 }
 
 // Beispielaufruf der Funktion
 versioniereDaten({
     id: 1,
     inhalt: 'Beispieldaten'
+}, () => {
+    console.log('Post-Versionierung abgeschlossen.');
 });
